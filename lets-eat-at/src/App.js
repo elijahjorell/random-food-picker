72b9becc9cbc88e0f0dd076aba4c4381713e68ca
Elijah Esmero <elijah.esmero@fdmgroup.com>
2021-02-01 23:56:10 +1100

    Complete first iteration

--- lets-eat-at/src/App.js
+++ lets-eat-at/src/App.js
@@ -1,31 +1,50 @@
 import './App.css';
 import {useEffect, useState} from "react/cjs/react.production.min";
 import GMap from "./GMap";
+import {Form} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    GMap.findPlaceFromText("Marsden Park").then((res) => {
-      console.log("findplacefromtext:");
-      console.log(res);
-    });
-    GMap.nearbySearch(-33.702382, 150.835362, 20000, "restaurant").then((res) => {
-      console.log("nearbysearch:");
-      console.log(res);
-      setData(res.results["0"]);
-    });
-    GMap.textsearch("Marsden Park KFC").then((res) => {
-      console.log("textsearch:");
-      console.log(res);
-    })
+    // axios.get("http://ip-api.com/json").then((res) => {
+    //   console.log(res.data)
+    // })
+    // GMap.findPlaceFromText("Marsden Park").then((res) => {
+    //   console.log("findplacefromtext:");
+    //   console.log(res);
+    // });
   }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted")
+
+    axios.get("http://ip-api.com/json").then((ipData) => {
+      console.log(ipData);
+      GMap.textsearch(event.target[0].value + " " + ipData.data.region + " " + ipData.data.country).then((searchData) => {
+        return searchData.results["0"]["geometry"]["location"];
+      }).then((location) => {
+        GMap.nearbySearch(location["lat"], location["lng"], 10000, "restaurant").then((res) => {
+          console.log(res);
+          setData(res.results["0"]);
+        });
+      })
+    });
+  }
 
   return (
     <div className="App">
       {
         data === undefined ? (
-            <h1>Let's eat around...</h1>
+            <div>
+              <h1>Let's eat around...</h1>
+              <Form onSubmit={handleSubmit}>
+                <Form.Control placeholder="Enter suburb"/>
+              </Form>
+            </div>
         ) : (
             <div>
               <h1>Let's eat at...</h1>
